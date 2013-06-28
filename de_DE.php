751c601e8cfba98d0d146cd6bcc808021ae224e0
Christian Foellmann <foellmann@foe-services.de>
2013-06-28 09:42:49 +0200

    secure from direct access

--- de_DE.php
+++ de_DE.php
@@ -20,6 +20,13 @@
  * avoid additional filtering at frontend html generation
  */
 
+//avoid direct calls to this file
+if ( ! function_exists( 'add_filter' ) ) {
+	header('Status: 403 Forbidden');
+	header('HTTP/1.1 403 Forbidden');
+	exit();
+}
+
 /**
  * Check for the plugin Germanizer with the same topic and doing nothing
  * 
